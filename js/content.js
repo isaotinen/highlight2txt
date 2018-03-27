c23099d291e132d9ce6071863a585d53a9a7425e
Isao Tinen <isaotinen@gmail.com>
2018-03-28 00:12:49 +0900

    関数化、その他諸々

--- js/content.js
+++ js/content.js
@@ -1,4 +1,23 @@
-(function() {
-  'use strict';
-  Array.from(document.querySelectorAll('div.aaa'), e=>alert(e.innerText));
-})();
+'use strict';
+
+var buttonTargetId = "test1"
+var createButton = document.createElement("button");
+createButton.innerText = "Show Highlights";
+createButton.onclick = showHighlight;
+var highlightArea = document.createElement("div");
+add(buttonTargetId, document.createElement("br"));
+add(buttonTargetId, createButton);
+add(buttonTargetId, document.createElement("br"));
+add(buttonTargetId, highlightArea);
+
+function add(id, tag){
+  document.getElementById(id).appendChild(tag);
+}
+
+function showHighlight() {
+  highlightArea.innerHTML = '<textarea id="highlightarea" name="contents" rows="30" cols="100"></textarea>';
+  Array.from(document.querySelectorAll('div.aaa'), function(e){
+    document.getElementById("highlightarea").innerText = e.innerText;
+  });
+}
+//テキストエリア作って書き込むとこまで。繰り返し処理すると最後の内容しか表示されない。
